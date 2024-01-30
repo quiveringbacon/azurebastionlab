214b670b5a87a01c0fd4a7013aab8723782fbd5e
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-01-30 07:42:23 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,464 @@
+provider "azurerm" {
+  features {
+  }
+}
+
+#variables
+variable "A-location" {
+    description = "Location of the resources, example: eastus2"
+    #default     = "eastus"
+}
+
+variable "B-resource_group_name" {
+    description = "Name of the resource group to create"
+}
+
+variable "C-home_public_ip" {
+    description = "Your home public ip address"
+}
+
+variable "D-username" {
+    description = "Username for Virtual Machines"
+    #default     = "azureuser"
+}
+
+variable "E-password" {
+    description = "Password for Virtual Machines"
+    sensitive = true
+}
+
+resource "azurerm_resource_group" "RG" {
+  location = var.A-location
+  name     = var.B-resource_group_name  
+}
+
+#logic app to self destruct resourcegroup after 24hrs
+data "azurerm_subscription" "sub" {
+}
+
+resource "azurerm_logic_app_workflow" "workflow1" {
+  location = azurerm_resource_group.RG.location
+  name     = "labdelete"
+  resource_group_name = azurerm_resource_group.RG.name
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [
+    azurerm_resource_group.RG,
+  ]
+}
+resource "azurerm_role_assignment" "contrib1" {
+  scope = azurerm_resource_group.RG.id
+  role_definition_name = "Contributor"
+  principal_id  = azurerm_logic_app_workflow.workflow1.identity[0].principal_id
+  depends_on = [azurerm_logic_app_workflow.workflow1]
+}
+
+resource "azurerm_resource_group_template_deployment" "apiconnections" {
+  name                = "group-deploy"
+  resource_group_name = azurerm_resource_group.RG.name
+  deployment_mode     = "Incremental"
+  template_content = <<TEMPLATE
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {},
+    "variables": {},
+    "resources": [
+        {
+            "type": "Microsoft.Web/connections",
+            "apiVersion": "2016-06-01",
+            "name": "arm-1",
+            "location": "${azurerm_resource_group.RG.location}",
+            "kind": "V1",
+            "properties": {
+                "displayName": "labdeleteconn1",
+                "authenticatedUser": {},
+                "statuses": [
+                    {
+                        "status": "Ready"
+                    }
+                ],
+                "connectionState": "Enabled",
+                "customParameterValues": {},
+                "alternativeParameterValues": {},
+                "parameterValueType": "Alternative",
+                "createdTime": "2023-05-21T23:07:20.1346918Z",
+                "changedTime": "2023-05-21T23:07:20.1346918Z",
+                "api": {
+                    "name": "arm",
+                    "displayName": "Azure Resource Manager",
+                    "description": "Azure Resource Manager exposes the APIs to manage all of your Azure resources.",
+                    "iconUri": "https://connectoricons-prod.azureedge.net/laborbol/fixes/path-traversal/1.0.1552.2695/arm/icon.png",
+                    "brandColor": "#003056",
+                    "id": "/subscriptions/${data.azurerm_subscription.sub.subscription_id}/providers/Microsoft.Web/locations/${azurerm_resource_group.RG.location}/managedApis/arm",
+                    "type": "Microsoft.Web/locations/managedApis"
+                },
+                "testLinks": []
+            }
+        },
+        {
+            "type": "Microsoft.Logic/workflows",
+            "apiVersion": "2017-07-01",
+            "name": "labdelete",
+            "location": "${azurerm_resource_group.RG.location}",
+            "dependsOn": [
+                "[resourceId('Microsoft.Web/connections', 'arm-1')]"
+            ],
+            "identity": {
+                "type": "SystemAssigned"
+            },
+            "properties": {
+                "state": "Enabled",
+                "definition": {
+                    "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+                    "contentVersion": "1.0.0.0",
+                    "parameters": {
+                        "$connections": {
+                            "defaultValue": {},
+                            "type": "Object"
+                        }
+                    },
+                    "triggers": {
+                        "Recurrence": {
+                            "recurrence": {
+                                "frequency": "Minute",
+                                "interval": 3,
+                                "startTime": "${timeadd(timestamp(),"24h")}"
+                            },
+                            "evaluatedRecurrence": {
+                                "frequency": "Minute",
+                                "interval": 3,
+                                "startTime": "${timeadd(timestamp(),"24h")}"
+                            },
+                            "type": "Recurrence"
+                        }
+                    },
+                    "actions": {
+                        "Delete_a_resource_group": {
+                            "runAfter": {},
+                            "type": "ApiConnection",
+                            "inputs": {
+                                "host": {
+                                    "connection": {
+                                        "name": "@parameters('$connections')['arm']['connectionId']"
+                                    }
+                                },
+                                "method": "delete",
+                                "path": "/subscriptions/@{encodeURIComponent('${data.azurerm_subscription.sub.subscription_id}')}/resourcegroups/@{encodeURIComponent('${azurerm_resource_group.RG.name}')}",
+                                "queries": {
+                                    "x-ms-api-version": "2016-06-01"
+                                }
+                            }
+                        }
+                    },
+                    "outputs": {}
+                },
+                "parameters": {
+                    "$connections": {
+                        "value": {
+                            "arm": {
+                                "connectionId": "[resourceId('Microsoft.Web/connections', 'arm-1')]",
+                                "connectionName": "arm-1",
+                                "connectionProperties": {
+                                    "authentication": {
+                                        "type": "ManagedServiceIdentity"
+                                    }
+                                },
+                                "id": "/subscriptions/${data.azurerm_subscription.sub.subscription_id}/providers/Microsoft.Web/locations/${azurerm_resource_group.RG.location}/managedApis/arm"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    ]
+}
+TEMPLATE
+}
+
+#vnets and subnets
+resource "azurerm_virtual_network" "hub-vnet" {
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  name                = "AZ-hub-vnet"
+  resource_group_name = azurerm_resource_group.RG.name
+  subnet {
+    address_prefix     = "10.0.0.0/24"
+    name                 = "default"
+    security_group = azurerm_network_security_group.hubvnetNSG.id
+  }
+  subnet {
+    address_prefix     = "10.0.1.0/24"
+    name                 = "GatewaySubnet" 
+  }
+  subnet{
+    address_prefix = "10.0.2.0/24"
+    name = "AzureBastionSubnet"
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+}
+resource "azurerm_virtual_network" "spoke-vnet" {
+  address_space       = ["10.250.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  name                = "AZ-spoke-vnet"
+  resource_group_name = azurerm_resource_group.RG.name
+  subnet {
+    address_prefix     = "10.250.0.0/24"
+    name                 = "default"
+    security_group = azurerm_network_security_group.spokevnetNSG.id
+  }
+  subnet {
+    address_prefix     = "10.250.1.0/24"
+    name                 = "GatewaySubnet" 
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+
+resource "azurerm_virtual_network_peering" "hubtospokepeering" {
+  name                      = "hub-to-spoke-peering"
+  remote_virtual_network_id = azurerm_virtual_network.spoke-vnet.id
+  resource_group_name       = azurerm_resource_group.RG.name
+  virtual_network_name      = "AZ-hub-vnet"
+  allow_forwarded_traffic = true
+  allow_gateway_transit = true
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  depends_on = [
+    azurerm_virtual_network.hub-vnet,
+    azurerm_virtual_network.spoke-vnet,
+    
+  ]
+}
+resource "azurerm_virtual_network_peering" "spoketohubpeering" {
+  name                      = "spoke-to-hub-peering"
+  remote_virtual_network_id = azurerm_virtual_network.hub-vnet.id
+  resource_group_name       = azurerm_resource_group.RG.name
+  virtual_network_name      = "AZ-spoke-vnet"
+  allow_forwarded_traffic = true
+  
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  depends_on = [
+    azurerm_virtual_network.spoke-vnet,
+    azurerm_virtual_network.hub-vnet,
+    
+  ]
+}
+
+#NSG's
+resource "azurerm_network_security_group" "hubvnetNSG" {
+  location            = azurerm_resource_group.RG.location
+  name                = "AZ-hub-vnet-default-nsg"
+  resource_group_name = azurerm_resource_group.RG.name
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+
+resource "azurerm_network_security_group" "spokevnetNSG" {
+  location            = azurerm_resource_group.RG.location
+  name                = "AZ-spoke-vnet-default-nsg"
+  resource_group_name = azurerm_resource_group.RG.name
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+
+
+
+#Public IP's
+resource "azurerm_public_ip" "bastion-pip" {
+  name                = "bastion-pip"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  allocation_method = "Static"
+  sku = "Standard"
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+
+#Bastion host
+resource "azurerm_bastion_host" "bastion1" {
+  name = "Bastion1"
+  location = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  sku = "Standard"
+  ip_connect_enabled = true
+  shareable_link_enabled = true
+  tunneling_enabled = true
+  ip_configuration {
+    name = "config1"
+    subnet_id = azurerm_virtual_network.hub-vnet.subnet.*.id[2]
+    public_ip_address_id = azurerm_public_ip.bastion-pip.id
+  }
+}
+
+#vNIC's
+resource "azurerm_network_interface" "hubvm-nic" {
+  location            = azurerm_resource_group.RG.location
+  name                = "hubvm-nic"
+  resource_group_name = azurerm_resource_group.RG.name
+  ip_configuration {
+    name                          = "ipconfig1"
+    private_ip_address_allocation = "Dynamic"    
+    subnet_id                     = azurerm_virtual_network.hub-vnet.subnet.*.id[0]
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+resource "azurerm_network_interface" "spokevm-nic" {
+  location            = azurerm_resource_group.RG.location
+  name                = "spokevm-nic"
+  resource_group_name = azurerm_resource_group.RG.name
+  ip_configuration {
+    name                          = "ipconfig1"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_virtual_network.spoke-vnet.subnet.*.id[0]
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+
+
+#VM's
+resource "azurerm_windows_virtual_machine" "hubvm" {
+  admin_password        = var.E-password
+  admin_username        = var.D-username
+  location              = azurerm_resource_group.RG.location
+  name                  = "hubvm"
+  network_interface_ids = [azurerm_network_interface.hubvm-nic.id]
+  resource_group_name   = azurerm_resource_group.RG.name
+  size                  = "Standard_B2ms"
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+resource "azurerm_virtual_machine_extension" "killhubvmfirewall" {
+  auto_upgrade_minor_version = true
+  name                       = "killhubvmfirewall"
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  virtual_machine_id         = azurerm_windows_virtual_machine.hubvm.id
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -command \"Set-NetFirewallProfile -Enabled False\""
+    }
+  SETTINGS
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+resource "azurerm_windows_virtual_machine" "spokevm" {
+  admin_password        = var.E-password
+  admin_username        = var.D-username
+  location              = azurerm_resource_group.RG.location
+  name                  = "spokevm"
+  network_interface_ids = [azurerm_network_interface.spokevm-nic.id]
+  resource_group_name   = azurerm_resource_group.RG.name
+  size                  = "Standard_B2ms"
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
+resource "azurerm_virtual_machine_extension" "killspokevmfirewall" {
+  auto_upgrade_minor_version = true
+  name                       = "killspokevmfirewall"
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  virtual_machine_id         = azurerm_windows_virtual_machine.spokevm.id
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -command \"Set-NetFirewallProfile -Enabled False\""
+    }
+  SETTINGS
+  timeouts {
+    create = "2h"
+    read = "2h"
+    update = "2h"
+    delete = "2h"
+  }
+  
+}
